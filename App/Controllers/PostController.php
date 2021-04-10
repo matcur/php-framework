216a389ed6fedcceafdf2a1908e5c860a1a18f84
matcur <ru.far2014@yandex.ru>
2021-04-10 11:10:43 +0300

    Добавил классы для кеширование и работы с файлами.

--- App/Controllers/PostController.php
+++ App/Controllers/PostController.php
@@ -6,19 +6,35 @@ use App\Models\Post;
 use Framework\ActionResults\Json;
 use Framework\ActionResults\Redirect;
 use Framework\ActionResults\View;
+use Framework\Caching\Cache;
 use Framework\Controller\BaseController;
 
 class PostController extends BaseController
 {
+    /**
+     * @var Cache
+     */
+    private $cache;
+
+    public function __construct()
+    {
+        $this->cache = new Cache();
+    }
+
     public function actionIndex()
     {
-        $posts = [
-            new Post(1, 'first'),
-            new Post(2, 'second'),
-            new Post(3, 'third'),
-        ];
+        $posts = $this->cache->remember('posts', function () {
+            return [
+                new Post(1, 'first'),
+                new Post(2, 'second'),
+                new Post(3, 'third' . rand(1, 20)),
+            ];
+        });
 
-        return new View('post/index.php', compact('posts'));
+        return new View(
+            'post/index.php',
+            compact('posts')
+        );
     }
 
     public function actionShow()
---
+++ framework/FileSystem/File.php
@@ -0,0 +1,58 @@
+<?php
+
+
+namespace Framework\FileSystem;
+
+
+class File
+{
+    /**
+     * @var string
+     */
+    private $path;
+
+    public function __construct(string $path)
+    {
+        $this->path = $path;
+    }
+
+    public function write(string $content): File
+    {
+        file_put_contents($this->path, $content);
+
+        return $this;
+    }
+
+    public function read()
+    {
+        return file_get_contents($this->path);
+    }
+
+    public function flush(): File
+    {
+        $file = fopen($this->path, 'r+');
+
+        fflush($file);
+        fclose($file);
+
+        return $this;
+    }
+
+    public function remove()
+    {
+        unlink($this->path);
+    }
+
+    public function exists()
+    {
+        return file_exists($this->path);
+    }
+
+    public function ensureExisting()
+    {
+        if (file_exists($this->path))
+            return;
+
+        file_put_contents($this->path, '');
+    }
+}
