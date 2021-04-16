e83dce5c0a39c66b3281c830cada0a212a8ed8c5
matcur <ru.far2014@yandex.ru>
2021-04-16 13:26:16 +0300

    Добавил интерфейс Cache

--- framework/Caching/Cache.php
+++ framework/Caching/Cache.php
@@ -2,55 +2,15 @@
 
 namespace Framework\Caching;
 
-use Framework\FileSystem\File;
-
-class Cache
+interface Cache
 {
-    public function getFolder(): string
-    {
-        return $_SERVER['DOCUMENT_ROOT'] . '/../private/cache/';
-    }
+    public function store(string $key, $value);
 
-    public function store(string $key, $value)
-    {
-        $file = $this->makeFile($key);
+    public function get(string $key);
 
-        $file->ensureExisting();
-        $file->flush()
-             ->write(json_encode($value));
-    }
+    public function forget(string $key);
 
-    public function get(string $key)
-    {
-        $content = $this->makeFile($key)
-                        ->read();
+    public function has(string $key): bool;
 
-        return json_decode($content);
-    }
-
-    public function forget(string $key)
-    {
-        $this->makeFile($key)->remove();
-    }
-
-    public function has(string $key): bool
-    {
-        return $this->makeFile($key)->exists();
-    }
-
-    public function remember(string $key, callable $callback)
-    {
-        if ($this->has($key))
-            return $this->get($key);
-
-        $value = $callback();
-        $this->store($key, $value);
-
-        return $value;
-    }
-
-    private function makeFile(string $key): File
-    {
-        return new File($this->getFolder() . $key . '.txt');
-    }
+    public function remember(string $key, callable $callback);
 }
---
+++ framework/Caching/FileCache.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Framework\Caching;
+
+use Framework\FileSystem\File;
+
+class FileCache implements Cache
+{
+    /**
+     * @var string
+     */
+    private $folder;
+
+    public function __construct(string $folder = null)
+    {
+        $this->folder = $folder?? $_SERVER['DOCUMENT_ROOT'] . '/../private/cache/';
+    }
+
+    public function store(string $key, $value)
+    {
+        $file = $this->makeFile($key);
+
+        $file->ensureExisting();
+        $file->flush()
+             ->write(json_encode($value));
+    }
+
+    public function get(string $key)
+    {
+        $content = $this->makeFile($key)
+                        ->read();
+
+        return json_decode($content);
+    }
+
+    public function forget(string $key)
+    {
+        $this->makeFile($key)->remove();
+    }
+
+    public function has(string $key): bool
+    {
+        return $this->makeFile($key)->exists();
+    }
+
+    public function remember(string $key, callable $callback)
+    {
+        if ($this->has($key))
+            return $this->get($key);
+
+        $value = $callback();
+        $this->store($key, $value);
+
+        return $value;
+    }
+
+    private function makeFile(string $key): File
+    {
+        return new File($this->folder . $key . '.txt');
+    }
+}
